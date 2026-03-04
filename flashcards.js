461fe16d724ee79863e2a17fe8724b666a5a8540
Luke Linington <lukelinington@gmail.com>
2026-03-04 18:47:01 +0000

    add ability to cycle through flashcards in set

--- flashcards.js
+++ flashcards.js
@@ -6,19 +6,19 @@ async function flashcardClick(){
     }
 
     let flashcard = document.querySelector(".flashcard");
-    let showingQuestion = flashcard.textContent.trim() === flashcardContent[0].question;
+    let showingQuestion = flashcard.textContent.trim() === flashcardContent[flashcard_num].question;
 
     if (showingQuestion) {
-        flashcard.textContent = flashcardContent[0].answer;
-        console.log(flashcardContent[0].answer);
+        flashcard.textContent = flashcardContent[flashcard_num].answer;
+        console.log(flashcardContent[flashcard_num].answer);
 
     } else {
-        flashcard.textContent = flashcardContent[0].question;
-        console.log(flashcardContent[0].question);
+        flashcard.textContent = flashcardContent[flashcard_num].question;
+        console.log(flashcardContent[flashcard_num].question);
     }
     console.log("Flashcard clicked");
 }
-
+let flashcard_num = 0;
 async function getText(){
     let flashcardContent = await fetch('http://localhost:3000/api/cards');
     flashcardContent = await flashcardContent.json();
@@ -26,7 +26,7 @@ async function getText(){
         return "";
     }
 
-    let flashcardWords = flashcardContent[0].question;
+    let flashcardWords = flashcardContent[flashcard_num].question;
     return flashcardWords;
 }
 
@@ -41,4 +41,27 @@ document.addEventListener("DOMContentLoaded", async () => {
     flashcard.textContent = flashcardWords || "No flashcards available.";
     flashcard.hidden = false;
 });
+
+async function flashcardNext(){
+    let flashcardContent = await fetch('http://localhost:3000/api/cards');
+    flashcardContent = await flashcardContent.json();
+    if (flashcard_num < flashcardContent.length - 1) {
+        flashcard_num++;
+    }
+    await getText();
+    let flashcard = document.querySelector(".flashcard");
+    flashcard.textContent = flashcardContent[flashcard_num].question;
+
+}
+
+async function flashcardPrevious(){
+    let flashcardContent = await fetch('http://localhost:3000/api/cards');
+    flashcardContent = await flashcardContent.json();
+    if (flashcard_num > 0) {
+        flashcard_num--;
+    }
+    await getText();
+    let flashcard = document.querySelector(".flashcard");
+    flashcard.textContent = flashcardContent[flashcard_num].question;
+}
 
