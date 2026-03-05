587c0450a07be05649624719ec9b3408b955df4a
Luke Linington <lukelinington@gmail.com>
2026-03-05 18:54:40 +0000

    made it so that you can navigate between example sets I added earlier

--- flashcards.js
+++ flashcards.js
@@ -1,5 +1,5 @@
 async function flashcardClick(){
-    let flashcardContent = await getFlashcard();
+    let flashcardContent = await getFlashcard(set_id);
     if (!flashcardContent.length) {
         return;
     }
@@ -18,8 +18,11 @@ async function flashcardClick(){
     console.log("Flashcard clicked");
 }
 let flashcard_num = 0;
+const params = new URLSearchParams(window.location.search);
+const set_id = params.get('setId');
 async function getText(){
-    let flashcardContent = await getFlashcard();
+    
+    let flashcardContent = await getFlashcard(set_id);
     if (!flashcardContent.length) {
         return "";
     }
@@ -41,7 +44,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 async function flashcardNext(){
-    let flashcardContent = await getFlashcard()
+    let flashcardContent = await getFlashcard(set_id);
     if (flashcard_num < flashcardContent.length - 1) {
         flashcard_num++;
     }
@@ -51,20 +54,22 @@ async function flashcardNext(){
 
 }
 
-async function getFlashcard(){
-    let flashcardContent = await fetch('http://localhost:3000/api/cards');
+async function getFlashcard(set_id){
+    let flashcardContent = await fetch(`http://localhost:3000/api/cards?`);
     flashcardContent = await flashcardContent.json();
+    flashcardContent = flashcardContent.filter(card => card.set_id == set_id);
+
     return flashcardContent;
 }
 
 async function numFlashCardDisplay(){
     let text = document.querySelector("#flashcard-number");
-    text.textContent = flashcard_num + 1 + "/" + await getFlashcard().then(cards => cards.length);
+    text.textContent = flashcard_num + 1 + "/" + await getFlashcard(set_id).then(cards => cards.length);
     
 }
 
 async function flashcardPrevious(){
-    let flashcardContent = await getFlashcard();
+    let flashcardContent = await getFlashcard(set_id);
     if (flashcard_num > 0) {
         flashcard_num--;
     }
