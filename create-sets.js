4cfddb0407e65f0f93aaa30eca7eaa4307a5c1d7
Luke Linington <lukelinington@gmail.com>
2026-03-19 12:10:48 +0000

    make it so that each question and answer textbox appear one row for one set of questions and answers

--- create-sets.js
+++ create-sets.js
@@ -1,11 +1,15 @@
 function addFlashcard() {
+    const row = document.createElement("div");
+    row.className = "flashcard-row";
     const question = document.createElement("input");
     question.type = "text";
     question.placeholder = "Question";
-    document.querySelector(".container").appendChild(question);
     const answer = document.createElement("input");
     answer.type = "text";
     answer.placeholder = "Answer";
-    document.querySelector(".container").appendChild(answer);
+    row.appendChild(question);
+    row.appendChild(answer);
+    document.querySelector(".container").appendChild(row);
+
     
-}
+} 
